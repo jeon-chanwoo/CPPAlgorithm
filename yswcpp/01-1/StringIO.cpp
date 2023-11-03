bba1e02a5983ad109e919674d2cd399f57a7398c
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2023-11-03 12:08:24 +0900

    1.1

--- yswcpp/01-1/StringIO.cpp
+++ yswcpp/01-1/StringIO.cpp
@@ -5,10 +5,10 @@ int main(void)
     char name[100];
     char lang[200];
 
-    std::cout<<"이름은 무엇입니까?";
+    std::cout<<"이름은 무엇입니까?"<<std::endl;
     std::cin>>name;
 
-    std::cout<<"좋아하는 프로그래밍 언어는 무엇입니까?";
+    std::cout<<"좋아하는 프로그래밍 언어는 무엇입니까?"<<std::endl;
     std::cin>>lang;
 
     std::cout<<"내 이름은 "<<name<<"입니다.\n";
---
+++ yswcpp/01-5/01-5M/SimpleMain.cpp
@@ -0,0 +1,8 @@
+#include "SimpleFunc.h"
+
+int main(void)
+{
+    BestComImp1::SimpleFunc();
+    ProgComImp1::SimpleFunc();
+    return 0;
+}
