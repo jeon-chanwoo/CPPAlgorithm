61d048dea056e3ad605a491dbf701eb0d8525bcf
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2023-11-08 15:45:38 +0900

    01

---
+++ yswcpp/01/01-5/01-5M/SimpleFunc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "SimpleFunc.h"
+
+void BestComImp1::SimpleFunc(void)
+{
+    std::cout<<"BestCom이 정의한 함수"<<std::endl;
+}
+
+void ProgComImp1::SimpleFunc(void)
+{
+    std::cout<<"ProgCom이 정의한 함수"<<std::endl;
+}
---
+++ yswcpp/01/01-5/01-5M/SimpleFunc.h
@@ -0,0 +1,8 @@
+namespace BestComImp1
+{
+    void SimpleFunc(void);    
+}
+namespace ProgComImp1
+{
+    void SimpleFunc(void);
+}
