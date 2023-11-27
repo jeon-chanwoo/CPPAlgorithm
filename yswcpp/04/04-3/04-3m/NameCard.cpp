f0003cb6eaee5d25c13233615b36c9fc989fa728
jeon-chanwoo <63829000+jeon-chanwoo@users.noreply.github.com>
2023-11-27 12:25:23 +0900

    04-3m

---
+++ yswcpp/04/04-3/04-3m/NameCard.cpp
@@ -0,0 +1,54 @@
+#include "NameCard.h"
+#include <iostream>
+#include <cstring>
+#pragma warning(disable:4996)
+using namespace std;
+
+
+
+
+void COMP_POS::ShowPositionInfo(int pos)
+{
+	switch (pos)
+	{
+	case CLERK:
+		cout << "사원" << endl;
+		break;
+	case SENIOR:
+		cout << "주임" << endl;
+		break;
+	case ASSIST:
+		cout << "대리" << endl;
+		break;
+	case MANAGER:
+		cout << "과장" << endl;
+	default:
+		break;
+	}
+}
+
+NameCard::NameCard(const char* _name,const char* _company,const char* _phone, int pos) : position(pos)
+{
+	name = new char[strlen(_name) + 1];
+	company = new char[strlen(_company) + 1];
+	phone = new char[strlen(_phone) + 1];
+	strcpy(name, _name);
+	strcpy(company, _company);
+	strcpy(phone, _phone);
+}
+
+void NameCard::ShowNameCardInfo()
+{
+	cout << "이름 : " << name << endl;
+	cout << "회사 : " << company << endl;
+	cout << "전화번호 : " << phone << endl;
+	cout << "직급 : "; COMP_POS::ShowPositionInfo(position);
+	cout << endl;
+}
+
+NameCard::~NameCard()
+{
+	delete[]name;
+	delete[]company;
+	delete[]phone;
+}
---
+++ yswcpp/04/04-3/04-3m/NameCard.h
@@ -0,0 +1,21 @@
+#ifndef __NAMECARD_H__
+#define __NAMECARD_H__
+
+namespace COMP_POS
+{
+	enum {CLERK, SENIOR, ASSIST, MANAGER};
+	void ShowPositionInfo(int pos);
+}
+
+class NameCard {
+public:
+	NameCard(const char* _name,const char* _company,const char* _phone, int pos);
+	void ShowNameCardInfo();
+	~NameCard();
+private:
+	char* name;
+	char* company;
+	char* phone;
+	int position;
+};
+#endif
